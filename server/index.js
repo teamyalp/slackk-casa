910a577d8e0d4e4989fb5fd639a57f9040427f15
Andy Dam <andy@andydam.com>
2018-01-10 09:13:18 -0800

    (bug) refactored code to follow heroku example for ws and express integration to avoid settings headers error (#7)

--- server/index.js
+++ server/index.js
@@ -4,14 +4,12 @@ const http = require('http');
 const WebSocket = require('ws');
 const { onConnect } = require('./webSocket');
 
-const app = express();
-app.use(router);
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer(app);
+const server = express()
+  .use(router)
+  .listen(PORT, () => console.log(`listenting on port ${PORT}`));
+
 const wss = new WebSocket.Server({ server });
-server.on('request', app);
 
 wss.on('connection', ws => onConnect(ws, wss));
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`listenting on port ${PORT}`));
