f2846cd720438d7e082fb2c3b8450902c09f8aed
Chase Lee <chaseklee990@gmail.com>
2018-01-11 14:43:35 -0800

    Feat/client fix ui & enter button (#22)
    * (feat) Implement send message on enter press
    
    * (refactor) Use key codes for enter for mobile compatability
    
    Delete some stuff in the render statements that were unnecessary
    
    * (feat) Add anchor chat scroll to bottom of list and fix ui layout
    
    * (feat) Fix layout to use full screen and messages to bottom of scroll
    
    * (bug) Fix the server route to be compatable with heroku

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { connect, sendMessage } from '../socketHelpers';
-import MessageList from './MessageList.jsx';
-import { Button } from 'reactstrap';
+import { Input } from 'reactstrap';
 import NavBar from './NavBar.jsx';
+import MessageList from './MessageList.jsx';
 import Body from './Body.jsx';
-import { Container, FormGroup, Label, Input } from 'reactstrap';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,45 +13,57 @@ export default class App extends React.Component {
       // users: [],
       query: '',
     };
+
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
   }
 
   componentDidMount() {
-    let server = 'wss://slackk-casa.herokuapp.com';
-    // let server = location.origin.replace(/^http/, 'ws');
+    // let server = 'wss://slackk-casa.herokuapp.com';
+    let server = location.origin.replace(/^http/, 'ws');
+
     // connect to the websocket server
     connect(server, this);
   }
 
-  // sends the input field value as a json object to the server socket
-  handleClick() {
-    console.log('username: ', this.props.location.state.username);
-    console.log('password: ', this.props.location.state.password);
-    sendMessage(this.state.query);
-  }
-
+  // changes the query state based on user input in text field
   handleChange(event) {
     this.setState({
       query: event.target.value,
     });
-    console.log(this.state.query);
+  }
+
+  // sends message on enter key pressed and clears form
+  // only when shift+enter pressed breaks to new line
+  handleKeyPress(event) {
+    // on key press enter send message and reset text box
+    if (event.charCode === 13 && !event.shiftKey) {
+      event.preventDefault();
+      sendMessage(this.state.query);
+      // resets text box to blank string
+      this.setState({
+        query: '',
+      });
+    }
   }
 
   render() {
-    let { messages } = this.state;
+    let { messages, query } = this.state;
     return (
-      <div>
+      <div className="app-container">
         <NavBar />
         <Body messages={messages} />
         <div className="input-container">
-          <FormGroup className="text-area">
-            <Label for="exampleText">Text Area</Label>
-            <Input type="textarea" name="text" id="exampleText" onChange={this.handleChange} />
-          </FormGroup>
+          {/* TODO: substitue main for current channel or DM user */}
+          <Input
+            value={query}
+            type="textarea"
+            name="text"
+            placeholder="Message #main"
+            onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress}
+          />
         </div>
-        <button className="chat-send-button" onClick={() => this.handleClick()}>
-          Send!
-        </button>
       </div>
     );
   }
