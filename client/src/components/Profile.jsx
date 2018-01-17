650e7200c4aaceba89ff323cf33dcb6350c2cbcb
kayfo23 <kayleigh.s.foley@gmail.com>
2018-01-16 19:36:23 -0800

    Create basic Profile navbar & form skeleton

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { 
+    Button,
+    Col,
+    Container,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Navbar, 
+    NavbarBrand,
+    NavItem,
+    Row
+} from 'reactstrap';
 
 export default class Profile extends React.Component {
     constructor(props) {
@@ -9,16 +21,38 @@ export default class Profile extends React.Component {
     }
 
     componentWillMount() {
-        console.log('testing for profile page render')
+        //get user data
     }
+
+    //get user data
 
     render() {
         return (
-            <div>
-                <Link to="/messages">
-                    <Button color="secondary">X</Button>
-                </Link>
-            </div>
+            <Container fluid className="profilePage">
+                <Row>
+                    <Col xs={8}>
+                        <Navbar expand="md">
+                            <NavbarBrand>Preferences</NavbarBrand>
+                            <NavItem>
+                                <Link to="/messages">X</Link>
+                            </NavItem>
+                        </Navbar>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={2}>
+                        Profile Image
+                    </Col>
+                    <Col xs={4}>
+                        <Form col-6>
+                            <FormGroup>
+                                <Label for="fullName">Full Name</Label>
+                                <Input type="text" id="fullName" placeholder="replace this with the current user's fullname" />
+                            </FormGroup>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 }
